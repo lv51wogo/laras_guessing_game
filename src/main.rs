9e4b1a72bc727ca7b473736a23bb763305e73ed8
larav <laravkries900@goolemail.com>
2022-12-21 19:38:46 +0100

    add rand and finish game

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
-    println!("Guess mynumber!");
+    println!("Guess my number!");
 
-    println!("Please input your number!");
+    let top_secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your number!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+         let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match 
+            guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&top_secret_number){
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too hight!"),
+            Ordering::Equal => {
+                println!("Yey you win!");
+                break;
+            }
+        }
+    }
 }
